1c846797e515ebe872e17954168fe86bde65217f
A-Barnett <91531818+A-Barnett@users.noreply.github.com>
2025-02-13 14:25:44 +0000

    Added Combiner

---
+++ Assets/Editor/ColliderCombinerEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ColliderCombiner))]
+public class ColliderCombinerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector(); // Draw default script properties
+
+        ColliderCombiner script = (ColliderCombiner)target;
+        if (GUILayout.Button("Combine Colliders"))
+        {
+            script.RunCombiner();
+            //
+        }
+    }
+}
